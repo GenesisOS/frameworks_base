3d984a0863fd5bd2e57d63fb53b872f513af79bd
Jon Evans <jon@craftyjon.com>
2014-11-30 20:55:42 -0500

    PowerManagerService: Wake on plug (1/2)
    Change-Id: I5e50a7fd351857d76c1c2fd13c6e737d8902a2f3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -20407,6 +20407,13 @@ public final class Settings {
                 "watch_ranging_available";
 
         /**
+         * Whether to wake the display when plugging or unplugging the charger
+         * @hide
+         */
+        public static final String WAKE_WHEN_PLUGGED_OR_UNPLUGGED =
+                "wake_when_plugged_or_unplugged";
+
+        /**
          * Settings migrated from Wear OS settings provider.
          * @hide
          */
