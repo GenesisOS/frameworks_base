ea56e94e50253fef27c5c499585b81a240a2d2bb
Danny Baumann <dannybaumann@web.de>
2019-07-03 15:12:22 +0200

    SystemUI: Add VPN tile
    Allows quickly connecting/disconnecting to/from VPNs.
    
    Co-authored-by: Trogel <trogel@clink.de>
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Iaa9413bf1071805be8764d92bc5e2dd3131fdaed

--- packages/SystemUI/src/com/android/systemui/genesis/GenesisModule.kt
+++ packages/SystemUI/src/com/android/systemui/genesis/GenesisModule.kt
@@ -26,6 +26,7 @@ import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
+import com.android.systemui.qs.tiles.VpnTile
 import com.android.systemui.qs.tiles.base.shared.model.QSTileConfig
 import com.android.systemui.qs.tiles.base.shared.model.QSTileUIConfig
 import com.android.systemui.res.R
@@ -74,6 +75,12 @@ interface GenesisModule {
     @StringKey(UsbTetherTile.TILE_SPEC)
     fun bindUsbTetherTile(usbTetherTile: UsbTetherTile): QSTileImpl<*>
 
+    /** Inject VpnTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(VpnTile.TILE_SPEC)
+    fun bindVpnTile(vpnTile: VpnTile): QSTileImpl<*>
+
     companion object {
         const val AMBIENT_DISPLAY_TILE_SPEC = "ambient_display"
         const val AOD_TILE_SPEC = "aod"
@@ -81,6 +88,7 @@ interface GenesisModule {
         const val HEADS_UP_TILE_SPEC = "heads_up"
         const val SYNC_TILE_SPEC = "sync"
         const val USB_TETHER_TILE_SPEC = "usb_tether"
+        const val VPN_TILE_SPEC = "vpn"
 
         @Provides
         @IntoMap
@@ -167,6 +175,21 @@ interface GenesisModule {
                     QSTileUIConfig.Resource(
                         iconRes = R.drawable.ic_qs_usb_tether,
                         labelRes = R.string.quick_settings_usb_tether_label
+                    ),
+                instanceId = uiEventLogger.getNewInstanceId(),
+                category = TileCategory.CONNECTIVITY,
+            )
+
+        @Provides
+        @IntoMap
+        @StringKey(VPN_TILE_SPEC)
+        fun provideVpnTileConfig(uiEventLogger: QsEventLogger): QSTileConfig =
+            QSTileConfig(
+                tileSpec = TileSpec.create(VPN_TILE_SPEC),
+                uiConfig =
+                    QSTileUIConfig.Resource(
+                        iconRes = R.drawable.ic_qs_vpn,
+                        labelRes = R.string.quick_settings_vpn_label
                     ),
                 instanceId = uiEventLogger.getNewInstanceId(),
                 category = TileCategory.CONNECTIVITY,
