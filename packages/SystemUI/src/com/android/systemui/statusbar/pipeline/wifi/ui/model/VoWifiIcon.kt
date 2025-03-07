503edcd07e42ce59e68eb72357e9812964424b56
COSMIC <rayavanindra@gmail.com>
2025-03-07 03:47:27 +0000

    SystemUI: Migrate to com.android.systemui.res.R

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/wifi/ui/model/VoWifiIcon.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/wifi/ui/model/VoWifiIcon.kt
@@ -16,7 +16,7 @@
 
 package com.android.systemui.statusbar.pipeline.wifi.ui.model
 
-import com.android.systemui.R
+import com.android.systemui.res.R
 import com.android.systemui.common.shared.model.Icon
 import com.android.systemui.statusbar.pipeline.wifi.shared.model.VoWifiState
 
