e70fe50bf03970af3c2cbdeb5cca8aacb1034589
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-07-11 22:41:44 +0530

    Smart5gService: Avoid early return with multiple subs
    When using dual sim with second sim as the active data sub, there can
    be a case where update() returns at first sub id which breaks smart 5G
    functionality. Use continue instead of return as intended.
    
    Change-Id: I4d70a2fcf3ec816a5f964c3a736841f33f6ca982

--- services/core/java/com/android/server/Smart5gService.java
+++ services/core/java/com/android/server/Smart5gService.java
@@ -237,7 +237,7 @@ public class Smart5gService extends SystemService {
         for (int subId : mActiveSubIds) {
             final TelephonyManager tm = mTelephonyManager.createForSubscriptionId(subId);
             final long supportedNrBitmask = getSupportedNrBitmask(tm, subId);
-            if (supportedNrBitmask == 0) return;
+            if (supportedNrBitmask == 0) continue;
             long allowedNetworkTypes = tm.getAllowedNetworkTypesForReason(
                     ALLOWED_NETWORK_TYPES_REASON_POWER);
             final boolean is5gAllowed = (allowedNetworkTypes & supportedNrBitmask) != 0;
@@ -249,7 +249,7 @@ public class Smart5gService extends SystemService {
             } else if (!shouldDisable && !is5gAllowed) {
                 allowedNetworkTypes |= supportedNrBitmask;
             } else {
-                return;
+                continue;
             }
             tm.setAllowedNetworkTypesForReason(ALLOWED_NETWORK_TYPES_REASON_POWER,
                     allowedNetworkTypes);
